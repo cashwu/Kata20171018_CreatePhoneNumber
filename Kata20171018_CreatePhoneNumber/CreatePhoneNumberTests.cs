6c28b629c434631aa31debf670795e60231650e4
cash <cash@cashwu.com>
2017-10-18 11:39:48 +0800

    refactor

--- Kata20171018_CreatePhoneNumber/CreatePhoneNumberTests.cs
+++ Kata20171018_CreatePhoneNumber/CreatePhoneNumberTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20171018_CreatePhoneNumber
@@ -10,8 +12,16 @@ namespace Kata20171018_CreatePhoneNumber
         public void PhoneNumberAll_1()
         {
             var kata = new Kata();
-            var result = kata.CreatePhoneNumber(new[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+            var result = kata.CreatePhoneNumber(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 });
             Assert.AreEqual("(111) 111-1111", result);
+        }
+
+        [TestMethod]
+        public void PhoneNumber()
+        {
+            var kata = new Kata();
+            var result = kata.CreatePhoneNumber(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
+            Assert.AreEqual("(123) 456-7890", result);
         }
     }
 
@@ -19,7 +29,12 @@ namespace Kata20171018_CreatePhoneNumber
     {
         public string CreatePhoneNumber(int[] numbers)
         {
-            return "(111) 111-1111";
+            return $"({JoinString(numbers, 0, 3)}) {JoinString(numbers, 3, 3)}-{JoinString(numbers, 6, 4)}";
+        }
+
+        private string JoinString(IEnumerable<int> nums, int skip, int take)
+        {
+            return string.Concat(nums.Skip(skip).Take(take));
         }
     }
 }
